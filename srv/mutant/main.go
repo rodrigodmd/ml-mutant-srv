2917bda034285a582c6b3e312f1b9274617ee427
Rodrigo Moreno <rodrigodmd@hotmail.com>
2018-10-03 23:53:12 -0300

    Kafka POC

--- srv/mutant/main.go
+++ srv/mutant/main.go
@@ -2,24 +2,37 @@ package main
 
 import (
 	"log"
-	//"github.com/micro/go-log"
-	"github.com/micro/go-micro"
-	"github.com/rodrigodmd/ml-mutant-srv/srv/mutant/handler"
+	"strconv"
+	"time"
 
+	"github.com/micro/cli"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-plugins/broker/kafka"
+	"github.com/rodrigodmd/ml-mutant-srv/srv/mutant/handler"
 	mutant "github.com/rodrigodmd/ml-mutant-srv/srv/mutant/proto/mutant"
-	// To enable rabbitmq plugin uncomment
-	//_ "github.com/micro/go-plugins/broker/rabbitmq"
-	// To enable googlepubsub plugin uncomment
-	//_ "github.com/micro/go-plugins/broker/googlepubsub"
-	// To enable kafka plugin uncomment
 	"github.com/rodrigodmd/ml-mutant-srv/srv/mutant/publisher"
 )
 
 func main() {
+	waitSeconds := 0
+	b := kafka.NewBroker()
+
 	// New Service
 	service := micro.NewService(
 		micro.Name("go.micro.srv.mutant"),
 		micro.Version("latest"),
+		micro.Broker(b),
+		micro.Flags(
+			cli.StringFlag{
+				Name:   "start_wait_time",
+				EnvVar: "START_WAIT_TIME",
+				Usage:  "Wait time to start microservice in seconds",
+				Value:  "0",
+			},
+		),
+		micro.Action(func(c *cli.Context) {
+			waitSeconds, _ = strconv.Atoi(c.String("start_wait_time"))
+		}),
 	)
 
 	// Initialise service
@@ -30,6 +43,9 @@ func main() {
 
 	// create publisher
 	publisher.NewPublisher(service.Client())
+
+	// Wait befor we start the service
+	time.Sleep(time.Second * time.Duration(waitSeconds))
 
 	// Run service
 	if err := service.Run(); err != nil {
--- srv/stats/main.go
+++ srv/stats/main.go
@@ -1,33 +1,30 @@
 package main
 
 import (
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/micro/cli"
 	"github.com/micro/go-log"
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/server"
+	"github.com/micro/go-plugins/broker/kafka"
 	"github.com/rodrigodmd/ml-mutant-srv/srv/stats/elastic"
 	"github.com/rodrigodmd/ml-mutant-srv/srv/stats/handler"
 	stats "github.com/rodrigodmd/ml-mutant-srv/srv/stats/proto/stats"
 	"github.com/rodrigodmd/ml-mutant-srv/srv/stats/subscriber"
-	// To enable rabbitmq plugin uncomment
-	//_ "github.com/micro/go-plugins/broker/rabbitmq"
-	// To enable googlepubsub plugin uncomment
-	//_ "github.com/micro/go-plugins/broker/googlepubsub"
-	// To enable kafka plugin uncomment
-	//_ "github.com/micro/go-plugins/broker/kafka"
-	//"github.com/micro/go-plugins/broker/kafka"
 )
 
 func main() {
-	// b := kafka.NewBroker()
+	waitSeconds := 0
+	b := kafka.NewBroker()
 
 	// New Service
 	service := micro.NewService(
 		micro.Name("go.micro.srv.stats"),
 		micro.Version("latest"),
-		// micro.Broker(b),
+		micro.Broker(b),
 		micro.Flags(
 			cli.StringFlag{
 				Name:   "elasticsearch_hosts",
@@ -35,8 +32,15 @@ func main() {
 				Usage:  "Comma separated list of elasticsearch hosts",
 				Value:  "localhost:9200",
 			},
+			cli.StringFlag{
+				Name:   "start_wait_time",
+				EnvVar: "START_WAIT_TIME",
+				Usage:  "Wait time to start microservice in seconds",
+				Value:  "0",
+			},
 		),
 		micro.Action(func(c *cli.Context) {
+			waitSeconds, _ = strconv.Atoi(c.String("start_wait_time"))
 			parts := strings.Split(c.String("elasticsearch_hosts"), ",")
 			elastic.Hosts = parts
 		}),
@@ -53,6 +57,9 @@ func main() {
 
 	// register subscriber with queue, each message is delivered to a unique subscriber
 	micro.RegisterSubscriber("go.micro.topic.mutant", service.Server(), subscriber.Handler, server.SubscriberQueue("queue.pubsub"))
+
+	// Wait befor we start the service
+	time.Sleep(time.Second * time.Duration(waitSeconds))
 
 	// Run service
 	if err := service.Run(); err != nil {
